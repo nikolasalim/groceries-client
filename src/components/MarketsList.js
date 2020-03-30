4b63047901bf73759a0b1b1da6562114b65e5656
nikolasalim <nikolasalim@gmail.com>
2020-03-30 13:14:48 +0200

    Fix markets sorting to automate the displaying

--- src/components/MarketsList.js
+++ src/components/MarketsList.js
@@ -1,84 +1,92 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// Getting user's current position:
+// // Getting user's current position:
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
+// const options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
 
-let coord = {
-  lat: null,
-  lng: null
-};
+// let coord = {
+//   lat: null,
+//   lng: null
+// };
 
-function success(pos) {
-  coord = {
-    latitude: pos.coords.latitude,
-    longitude: pos.coords.longitude
-  };
-  return coord;
-}
+// function success(pos) {
+//   coord = {
+//     latitude: pos.coords.latitude,
+//     longitude: pos.coords.longitude
+//   };
+//   return coord;
+// }
 
-function error(err) {
-  console.log("error is running");
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+// function error(err) {
+//   console.log("error is running");
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
 
-// navigator.geolocation.getCurrentPosition(success, error, options);
+// // navigator.geolocation.getCurrentPosition(success, error, options);
 
-// Getting distance between two coordenates:
+// // Getting distance between two coordenates:
 
-function getDistanceInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const d = R * c; // Distance in km
-  return d;
-}
+// function getDistanceInKm(lat1, lon1, lat2, lon2) {
+//   const R = 6371;
+//   const dLat = deg2rad(lat2 - lat1);
+//   const dLon = deg2rad(lon2 - lon1);
+//   const a =
+//     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//     Math.cos(deg2rad(lat1)) *
+//       Math.cos(deg2rad(lat2)) *
+//       Math.sin(dLon / 2) *
+//       Math.sin(dLon / 2);
+//   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//   const d = R * c; // Distance in km
+//   return d;
+// }
 
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
+// function deg2rad(deg) {
+//   return deg * (Math.PI / 180);
+// }
 
 class MarketsList extends React.Component {
-  state = { gotLocation: false };
+  // state = { gotLocation: false };
 
-  componentWillMount() {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-    setTimeout(() => this.setState({ gotLocation: true }), 3000);
-  }
+  // componentWillMount() {
+  //   navigator.geolocation.getCurrentPosition(success, error, options);
+  //   setTimeout(() => this.setState({ gotLocation: true }), 3000);
+  // }
 
   render() {
-    const marketsSorted = this.props.markets.sort((a, b) => {
-      a = getDistanceInKm(
-        coord.latitude,
-        coord.longitude,
-        a.latitude,
-        a.longitude
-      );
-      b = getDistanceInKm(
-        coord.latitude,
-        coord.longitude,
-        b.latitude,
-        b.longitude
-      );
-      return a - b;
-    });
+    // const marketsSorted = this.props.markets.sort((a, b) => {
+    //   a = getDistanceInKm(
+    //     coord.latitude,
+    //     coord.longitude,
+    //     a.latitude,
+    //     a.longitude
+    //   );
+    //   b = getDistanceInKm(
+    //     coord.latitude,
+    //     coord.longitude,
+    //     b.latitude,
+    //     b.longitude
+    //   );
+    //   return a - b;
+    // });
 
-    if (!this.state.gotLocation) {
-      return <h1>Loading...</h1>;
-    }
-    return marketsSorted.map(market => {
+    // if (!this.state.gotLocation) {
+    //   return <h1>Loading...</h1>;
+    // }
+    // return marketsSorted.map(market => {
+    //   return (
+    //     <Link to={`/market/${market.id}`} key={market.id}>
+    //       <div>{market.name}</div>
+    //     </Link>
+    //   );
+    // });
+
+    return this.props.markets.map(market => {
       return (
         <Link to={`/market/${market.id}`} key={market.id}>
           <div>{market.name}</div>
--- src/components/MarketsListContainer.js
+++ src/components/MarketsListContainer.js
@@ -4,24 +4,118 @@ import { getAllMarkets } from "../actions/marketsActions";
 import MarketsList from "./MarketsList";
 import SearchBarMarketsContainer from "./SearchBarMarketsContainer";
 
+// Getting user's current position:
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+let coord = {
+  lat: null,
+  lng: null
+};
+
+function success(pos) {
+  coord = {
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude
+  };
+  return coord;
+}
+
+function error(err) {
+  coord = {
+    latitude: "blocked",
+    longitude: "blocked"
+  };
+  console.log("error is running");
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+// Getting distance between two coordenates:
+
+function getDistanceInKm(lat1, lon1, lat2, lon2) {
+  const R = 6371;
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
 class MarketsListContainer extends React.Component {
+  // componentWillMount() {
+  //   navigator.geolocation.getCurrentPosition(success, error, options);
+  // }
+
   componentDidMount() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
     this.props.getAllMarkets();
+    // setTimeout(() => this.props.getAllMarkets(), 3000);
   }
 
+  componentDidUpdate() {
+    if (coord.lat === null || coord.long === null) {
+      this.props.getAllMarkets();
+    }
+  }
+
+  // Sorting markets by nearest location:
+
+  sortingMarkets = arr => {
+    const sorted = arr.sort((a, b) => {
+      a = getDistanceInKm(
+        coord.latitude,
+        coord.longitude,
+        a.latitude,
+        a.longitude
+      );
+      b = getDistanceInKm(
+        coord.latitude,
+        coord.longitude,
+        b.latitude,
+        b.longitude
+      );
+
+      return a - b;
+    });
+    // console.log("sorted is", sorted);
+    return sorted;
+  };
+
   render() {
+    if (this.props.markets.list.length === 0) {
+      return <h1>Loading...</h1>;
+    }
+
     if (this.props.markets.searched.length === 0) {
       return (
         <div>
           <SearchBarMarketsContainer />
-          <MarketsList markets={this.props.markets.list} />
+          <MarketsList markets={this.sortingMarkets(this.props.markets.list)} />
+          {/* <MarketsList markets={this.props.markets.list} /> */}
         </div>
       );
     }
     return (
       <div>
         <SearchBarMarketsContainer />
-        <MarketsList markets={this.props.markets.searched} />
+        <MarketsList
+          markets={this.sortingMarkets(this.props.markets.searched)}
+        />
+        {/* <MarketsList markets={this.props.markets.searched} /> */}
       </div>
     );
   }
